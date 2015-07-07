efa06a6405a1dcf9104b6b2e2264e775032fea3e
Jordan Sandoval <sndvljordan@aol.com>
2015-07-07 14:42:10 -0500

    Cleaner code with old code in comments

--- public/js/simonjs.js
+++ public/js/simonjs.js
@@ -1,21 +1,32 @@
 'use strict';
 
-var userInput = []
+var userIndex = 0;
 var gameSequence = [];
 
+
+//allow user input to have value
+$('.triangle').click(function(e){
+    var triangleClicked = $(this).attr('id');
+    animateTriangle(triangleClicked);
+    if (triangleClicked == gameSequence[userIndex]){
+        userIndex += 1;
+        if (userIndex == gameSequence.length){
+            copy();
+            userIndex = 0;
+        }
+    } else {
+            alert("Game Over");
+            location.reload(true);
+    }
+});
 //initiate game
 $('#square').click(function(e){
-    alert("Hi! I'm Simon!")
-    if (confirm('Want to play a game?')){
-            alert("Okay!");
-         } else{
-            alert('Please?')
-            alert('Yay!!')
-         }
-    alert("Get Ready!");
+    alert("Hi! I'm Simon! Let's play a game! Get Ready!")
     getRandomTriangle();
     playGame();
 });
+
+
 //animation function
 function animateTriangle(id) {
     $('#' + id).addClass('active');
@@ -23,6 +34,8 @@ function animateTriangle(id) {
         $('#' + id).removeClass('active');
     },250);
 };
+
+
 //grab triangle and input to game array
 function getRandomTriangle() {
     var triangle = $('.triangle');
@@ -31,6 +44,8 @@ function getRandomTriangle() {
     var id = buttonToAnimate.getAttribute('id');
     gameSequence.push(id);
 }
+
+
 //run game turn
 function playGame(){
     var i=0;
@@ -42,55 +57,32 @@ function playGame(){
         i+=1;
     },500);
 }
-//allow user input to have value
-$('.triangle').click(function(e){
-    var triangleClicked = $(this).attr('id');
-    animateTriangle(triangleClicked);
-    userInput.push(triangleClicked);
-    compareArrays();
 
 
-});
+
+
 //compare input in each
-function compareArrays() {
-    var sequenceMistake = false;
-    // check if simon and user arrays are the same
-    for (var i = 0; i < userInput.length; i++) {
-        // if they are not the same change sequenceMistake var to true and call game over funtion, else if they are the same call copy function and go another round 
-      if (gameSequence[i] != userInput[i]) {
-        sequenceMistake = true;
-        break;
-      }
-    }
-    if (sequenceMistake) {
-      alert("Game Over");
-      location.reload(true)
-    } else if (userInput.length == gameSequence.length) {
-      copy();
-    }
-}
+// function compareArrays() {
+//     var sequenceMistake = false;
+//     // check if simon and user arrays are the same
+//     for (var i = 0; i < userInput.length; i++) {
+//         // if they are not the same change sequenceMistake var to true and call game over funtion, else if they are the same call copy function and go another round 
+//       if (gameSequence[i] != userInput[i]) {
+//         sequenceMistake = true;
+//         break;
+//       }
+//     }
+//     if (sequenceMistake) {
+//       alert("Game Over");
+//       location.reload(true)
+//     } else if (userInput.length == gameSequence.length) {
+//       copy();
+//     }
+// }
+
+
 function copy() {
-    userInput = [];
     getRandomTriangle();
-    playback();
-}
-function playback() {
     $('#round').text('Round: '+ gameSequence.length);
-    var i = 0;
     playGame();
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
