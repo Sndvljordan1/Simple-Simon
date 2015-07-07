5ea101bf8909069a2dc9dacb6690438a9c45fafc
Jordan Sandoval <sndvljordan@aol.com>
2015-07-07 13:16:43 -0500

    finished simon

--- public/js/simonjs.js
+++ public/js/simonjs.js
@@ -1,70 +1,85 @@
 'use strict';
-// var detonationTimer = 5;
-// var intervalId = setInterval(function (){
-//     if (detonationTimer == 0) {
-//         alert('Game Over!');
-        
-//     // } else if (detonationTimer > 0) {
-//     //     document.getElementById('timer').innerHTML = detonationTimer;
-//     // }
-//     // detonationTimer--;
-//     }, 1000);
-// });
 
+var userInput = []
+var gameSequence = [];
 
-// var board = ['1','2','3','4'];
-// var game = [];
-// var random = Math.floor((Math.random()*5)+1);
-
-$('.triangle').click(function(e){
-    var triangleClicked = $(this);
-    triangleClicked.addClass('active');
-    setTimeout(function(e){
-        triangleClicked.removeClass('active');
-    },250);
-});
-
-// function getReady() {
-            
-//             alert('Get Ready!')
-//         };
-//         var invader = document.getElementById('defuser');
-//         defuser.addEventListener('click', defuseTheBOM, false);
-
+//initiate game
 $('#square').click(function(e){
+    alert("Hi! I'm Simon!")
+    if (confirm('Want to play a game?')){
+            alert("Okay!");
+         } else{
+            alert('Please?')
+            alert('Yay!!')
+         }
     alert("Get Ready!");
-    // $('#space-invader').fadeToggle();
-    
+    getRandomTriangle();
+    playGame();
 });
+//animation function
+function animateTriangle(id) {
+    $('#' + id).addClass('active');
+    setTimeout(function(){
+        $('#' + id).removeClass('active');
+    },250);
+};
+//grab triangle and input to game array
+function getRandomTriangle() {
+    var triangle = $('.triangle');
+    var rand = Math.floor(Math.random()*4);
+    var buttonToAnimate = triangle[rand];
+    var id = buttonToAnimate.getAttribute('id');
+    gameSequence.push(id);
+}
+//run game turn
+function playGame(){
+    var i=0;
+    var intervalId = setInterval(function(){
+        if(i>=gameSequence.length){
+            clearInterval(intervalId);
+        }
+        animateTriangle(gameSequence[i]);
+        i+=1;
+    },500);
+}
+//allow user input to have value
+$('.triangle').click(function(e){
+    var triangleClicked = $(this).attr('id');
+    animateTriangle(triangleClicked);
+    userInput.push(triangleClicked);
+    compareArrays();
 
 
+});
+//compare input in each
+function compareArrays() {
+    var sequenceMistake = false;
+    // check if simon and user arrays are the same
+    for (var i = 0; i < userInput.length; i++) {
+        // if they are not the same change sequenceMistake var to true and call game over funtion, else if they are the same call copy function and go another round 
+      if (gameSequence[i] != userInput[i]) {
+        sequenceMistake = true;
+        break;
+      }
+    }
+    if (sequenceMistake) {
+      alert("Game Over");
+      location.reload(true)
+    } else if (userInput.length == gameSequence.length) {
+      copy();
+    }
+}
+function copy() {
+    userInput = [];
+    getRandomTriangle();
+    playback();
+}
+function playback() {
+    $('#round').text('Round: '+ gameSequence.length);
+    var i = 0;
+    playGame();
 
-
-
-
-
-$(document).ready(function(){
-    var konamiCode = "38,38,40,40,37,39,37,39,66,65,13";
-    var code = [];
-    $(document).keyup(function(event){
-        console.log(event.keyCode);
-        code.push(event.keyCode);
-        if (code.toString().indexOf(konamiCode) >= 0){
-            $('body').css('color', 'chartreuse');
-            $('body').css('background-color', 'black');
-
-
-var board = ['1','2','3','4'];
-var game = [];
-var random = Math.floor((Math.random()*5)+1);
-
-game.push(random)
-var up = $('#up')
-
-
-
-
-
+}
 
 
 
